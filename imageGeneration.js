8a06b3d34723ffec96accf0daea6508d7e74a187
Theodor Hillmann <theo.hillmann@gmail.com>
2023-01-18 14:55:35 +0100

    initial backend

---
+++ imageGeneration.js
@@ -0,0 +1,34 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORGANISATION,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const listEngines = async () => {
+  const response = await openai.listEngines();
+  return response;
+};
+
+const generateImage = async (
+  prompt = "a white siamese cat",
+  pixel = 256,
+  number = 1
+) => {
+  console.log("Generating image...")
+  try {
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: number,
+      size: `${pixel}x${pixel}`,
+    });
+    const image_url = response.data.data[0].url;
+    return image_url;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { listEngines, generateImage };
